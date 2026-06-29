7b42f0d63fe88d2d9911bb616ce259eed358f92b
yogeshswami29 <ys4635023@gmail.com>
2026-06-29 20:57:16 +0530

    first commit

---
+++ yy.py
@@ -0,0 +1,864 @@
+#variable
+# var1="23"
+# print(var1)
+# var2="hello"
+# var3="world"
+# var4=(var2+var3)
+# print(var4)
+
+# age=18
+# print(age>=18)
+
+# an=8.9
+# print(an)
+# print(type(an))
+
+# age=15
+# print(age>=18)
+
+# age=16
+# print(type(age))
+
+#opraters
+#+add
+# var1="hello"
+# var2="world"
+# print(var1+var2)
+
+#sub
+#-
+# var1=7
+# var2=5
+# print(var1-var2)
+
+
+# var1=7
+# var2=5
+# print(var1*var2)
+
+
+# var1=7
+# var2=5
+# print(var1/var2)
+
+
+# var1=7
+# var2=5
+# print(var1%var2)
+
+
+# a=15
+# b=4
+# print(a/b)
+
+
+#exponentions
+# var1=2
+# var2=4
+# print(var1**var2)
+# var1=36
+# var2=3
+# print(var1+var2)
+
+
+# var1=33
+# var2=3
+# print(var1-var2)
+
+# var1=20
+# var2=10
+# print(var1*var2)
+
+# var1=10
+# var2=20
+# print(var1/var2)
+
+# var1=10
+# var2=22
+# print(var1**var2)
+
+# a=-10
+# b=3
+# result=a//b
+# print(result)
+
+# a=10
+# b=8
+# print(a%b)
+
+# a=10
+# b=3
+# print(a==b)
+# a=5
+# b=5
+# print(a==b)
+
+# a=10
+# b=10
+# result=(a==b)
+# print(result)
+
+# name1="yogesh"
+# name2="bfchvhc"
+# print(name1==name2)
+
+
+#not equal to
+
+# name="yogesh"
+# name="yogesh"
+# print(name!=name)
+# name2="yogesh"
+# name1="vishnu"
+# print(name2!=name1)
+
+#greaterthan
+# a=10
+# b=5
+# print(a>b)
+
+# a=10
+# b=20
+# print(a>b)
+
+# less than
+# a=10
+# b=5     
+# print(a<b)
+
+#greater than or equal to
+
+# a=10
+# b=5
+# print(a>=b)
+
+# a=10
+# b=5
+# result=(a>=b)
+# print(result)
+
+#lesstrhan or equal to
+# a=10
+# b=20
+# print(a<=b)
+
+#sum
+# a=10
+# b=90
+# result=(a+b)
+# print(result)
+
+#substraction
+# a=90
+# b=10
+# result=(a-b)
+# print(result)
+
+# multiplication
+# a=20
+# b=2
+# result=(a*b)
+# print(result)
+
+
+#division
+# a=20
+# b=10
+# result=(a/b)
+# print(result)
+
+#moduls
+# c=100
+# d=70
+# result=(c%d)
+# print(result)
+
+#floor divison
+# d=10
+# e=3
+# print(d//e)
+
+#power
+# a=2
+# b=3
+# print(a**b)
+
+
+#assignment operaters
+
+# a=20
+# a+=10
+# print(a)
+
+# a=200
+# a=a+100
+# print(a)
+
+#substract assign
+# a=20
+# a-=5
+# print(a)
+
+#multiply&assign
+# a=20
+# a*=2
+# print(a)
+
+#divide&assign
+# a=20
+# a/=4
+# print(a)
+
+# moduls&assign
+# a=17
+# a%=5
+# print(a)
+
+# a=20
+# b=22
+# result=(a+b)
+# print(result)
+
+# num=15
+# print(num>10 and num<20)
+# age=20
+# hasid = True
+# print(age >=18 and hasid)
+# age=16
+# has_id = True
+# print(age>=18 and has_id)
+ 
+# marks=75
+# attendence=85
+# print(marks >= 50 and attendence>=75)
+
+# day = "monday"
+# print(day=="sunday"or day =="monday")
+# marks =85
+# sports = False
+# print(marks >=90 or sports)
+# homeworkdone =False
+# print(not homeworkdone)
+
+# num=10
+# print(not(num>20))
+
+# age=20
+# print(not(age>22))
+
+# age=20
+# print(not(age>=18))
+
+# a=10
+# b=a
+# print(a is b)
+
+# x=[1,2]
+# y=[1,2]
+# print(x==y)         #== ye poochta hai ke data same hai kya ?
+# print(x is y)        #is poochta hai 
+
+# x=[2,2]
+# y=[4,2]
+# print(x==y)
+# print(x is x)
+
+#is not
+
+# a=[1,2,3]
+# b=[1,2,3]
+# print(a is not b)
+
+# x=10
+# y=10
+# print(x is not y)
+
+
+# x=20
+# y=30
+# print(x is not y)
+
+
+# membership operaters
+#check karte hai value collection me hai ya nhi 
+
+#1. in operaters
+# check karta hai ki value collection me hai ya nhi 
+
+# fruits=["apple","banana","mango"]
+# print("mango" in fruits)
+
+# name="yogesh"`                                                                                                `
+# print("g" in name)
+
+# numbers=["30","49","66"]
+# print("30" in numbers)
+
+# not
+
+# fruits=["apple","banana","mango"]
+# print("papaya" in fruits)
+
+# name="yogesh"
+# print("Y" in name)
+
+# numbers=["30","49","66"]
+# print("50" in numbers)
+
+
+# questions
+
+# ram=15
+# dost=10
+# total=(ram+dost)
+# print(total)
+
+# boys=25
+# girls=20
+# total=(boys+girls)
+# print(total)
+
+
+# subh=120
+# shaam=80
+# earning=(subh+shaam)
+# print(earning)
+
+# first=45
+# second=35
+# print(first+second)
+
+# jan=500
+# feb=650
+# print(jan+feb)
+
+
+# (-)
+# a=100
+# b=35
+# print(a-b)
+
+
+# bus
+# a=50
+# b=18
+# print(a-b)
+
+#school
+# a=800
+# b=125
+# print(a-b)
+
+#marks
+# a=95
+# b=12
+# print(a-b)
+
+#dukaan
+# a=300
+# b=120
+# print(a-b)
+
+
+# (*) Multiplication
+
+# a=10
+# b= 8 
+# print(a*b)
+
+# chocolates
+
+# a=12
+# b=6
+# print(a*b)
+
+# week
+
+# a=7
+# b=5
+# print(a*b)
+
+# chairs
+
+# a=15
+# b=4
+# print(a*b)
+
+# student
+
+# a=20
+# b=10
+# print(a*b)
+
+
+# chocolates
+
+
+# a=100
+# b=5
+# print(a/b)
+
+
+
+# rupaye
+
+
+# a=240
+# b=6
+# print(a/b)
+
+#  books
+
+
+# a=48
+# b=8
+# print(a/b)
+
+# students 
+
+
+# a=90
+# b=3
+# print(a/b)
+
+
+# a=150
+# b=10
+# print(a/b)
+
+# (%)modules
+
+# a=25
+# b=5
+# print(a%b)
+
+
+# a=37
+# b=5
+# print(a%b)
+
+
+
+# a=52
+# b=7
+# print(a%b)
+
+
+
+# a=100
+# b=9
+# print(a%b)
+
+
+
+# a=81
+# b=8
+# print(a%b)
+
+
+# (//)floor division
+
+# a=25
+# b=4
+# print(a//b)
+
+
+# a=37
+# b=5
+# print(a//b)
+
+
+# a=52
+# b=7
+# print(a//b)
+
+
+# a=100
+# b=9
+# print(a//b)
+
+
+# a=81
+# b=8
+# print(a//b)
+
+# (**) Power Operator
+
+# a=2
+# b=3
+# print(a**b)
+
+
+# (==) Equal To 
+# a=25
+# b=25
+# print(a==b)
+
+
+# a=100
+# b=100
+# print(a==b)
+
+# a=50
+# b=45
+# print(a==b)
+
+# a=12
+# b=12
+# print(a==b)
+
+# a=75
+# b=80
+# print(a==b)
+
+
+# (!=) Not Equal To
+
+# a=10
+# b=15
+# print(a!=b)
+
+
+# a=25
+# b=25
+# print(a!=b)
+
+
+# a=40
+# b=35
+# print(a!=b)
+
+
+# a=100
+# b=100
+# print(a!=b)
+
+
+# a=60
+# b=55
+# print(a!=b)
+
+
+# (>) Greater Than
+
+# a=20
+# b=15
+# print(a>b)
+
+
+# a=50
+# b=100
+# print(a>b)
+
+# a=90
+# b=45
+# print(a>b)
+
+# a=12
+# b=20
+# print(a>b)
+
+# a=60
+# b=55
+# print(a>b)
+
+
+# (<) Less Than
+
+# a=10
+# b=20
+# print(a<b)
+
+# a=50
+# b=30
+# print(a<b)
+
+# a=15
+# b=25
+# print(a<b)
+
+# a=80
+# b=100
+# print(a<b)
+
+# a=60
+# b=60
+# print(a<b)
+
+# (>=) Greater Than Equal To
+
+# a=25
+# b=20
+# print(a>=b)
+
+
+# a=50
+# b=50
+# print(a>=b)
+
+# a=15
+# b=25
+# print(a>=b)
+
+# a=100
+# b=90
+# print(a>=b)
+
+# a=70
+# b=70
+# print(a>=b)
+
+
+# # (<=) Less Than Equal To
+
+# a=15
+# b=20
+# print(a<=b)
+
+
+# a=50
+# b=50
+# print(a<=b)
+
+
+# a=80
+# b=60
+# print(a<=b)
+
+
+# a=25
+# b=30
+# print(a<=b)
+
+
+# a=70
+# b=70
+# print(a<=b)
+ 
+#  Assignment Operators
+# (=)
+
+# a=50
+# print(a)
+
+
+# age=17
+# print(age)
+
+# a=50
+# print(a)
+
+# school="arvind sr. sec. skr."
+# print(school)
+
+# a=999
+# print(a)
+
+# city="chommu"
+
+
+# a=("apple","banana","mango")
+# print("banana"in a)
+
+
+# a=("apple","banana","mango")
+# print("banana" not in a)
+
+
+# for i in range(10):
+#     print("yogesh")
+
+# for a in range(20):
+#     print("yogesh")
+
+
+# for i in range (5):
+#     print(i)
+
+# for i in range (1,11):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i)
+
+# sum=0
+# for i in range (1,11):
+#  sum=sum+i
+# print(sum)
+
+# sum=0
+# for i in range (1,11):
+#  sam=sum+i
+# print(sam)
+
+# for i in range (-10,0,1):
+    # print(i)
+
+# for i in range (-10,1):
+    # print(i)
+
+# for i in range (-10,0,1):
+#     print(i)
+
+# for i in range (-100,0):
+    # print(i)
+
+# a=1
+# b=21
+# for i in range (a,b):
+#     print(i)
+
+# a=-20
+# b=0
+# for i in range (a,b):
+#     print(i)
+
+# lst=['hello','my',5,True]
+# for i in lst:
+#     print(lst)
+
+
+# lst=['hello','my',5,True]
+# for i in lst:
+#     print(i)
+
+# lst=[3,45,12,3,67,9,11,86]
+# for i in lst:
+#     if i%2==0:
+#         print("the number is even ")
+
+#     else:
+#         print("the number is odd")
+
+# a="yogesh"
+# for i in (a):
+#     print(i)
+
+# b="yogesh00"
+# for i in (b):
+    # print(i)
+
+# my_str="yogesh"
+# for i in range (0,len(my_str),2):
+#     print(my_str[i:i+2])
+
+# a="yogesh"
+# for i in range (0,len(a),2):
+#     print(a[i:i+2])
+
+# a=" ytrsdfguhijojlhgfdfghjh"
+# for i in range (0,len(a),2):
+#     print(a[i:i+2])
+
+# for i in range (1,11) :
+#     print(i)
+ 
+
+# yogesh="yogesh"
+# for i in range (20) :
+    # print(yogesh)
+
+
+# yogesh="yogesh"
+# for i in range (2) :
+    # print(yogesh)
+
+
+# for i in range (0,10) :
+#     print(i)
+
+# sum=0
+# for i in range (1,11):
+#     sum=sum+i
+#     print(sum)
+
+# for i in range (-10,0,1):
+#     print(i)
+
+
+# a=0
+# b=10
+# for i in range (a,b):
+#     print(i)
+  
+
+#lst []
+# lst=['yogesh','i am ',3,]
+# for i in lst:
+#     print(i)
+
+
+# tuple ()
+
+# t=(1,2,3)
+
+# for i in t:
+#     print(i)
+
+# set {}
+# s={1,2,3}
+# for i in s:
+    # print(i)
+
+
+#break statement
+
+# for i in range(1,11):
+#     if i ==5:
+#         break
+#     print(i)
+
+
+# name="yogesh"
+# for i in name:
+#         if i=="e":
+#             break
+#         print(i)
+
+# for i in range (1,11):
+#     if i ==3:
+#         continue
+#     print(i)
+
+# for i in range (1,11):
+#     if i %3==0:
+#         continue
+#     print(i)
+
+    # name="yogesh"
+    # if i =="g":
+    #     continue
+    # print(i)
+
+
+# for i in range (5):
+#     pass
+# print("hello")
+
+# for i in range(5):
+    # print(i )
+    # pass
+
+
+# a="yogeshswami"
+# for i in range(0,len(a),2):
+#     print(a[i:i+2])
+
+# a="yogeshswami"
+# for i in range(0,len(a),5):
+#     print(a[i:i+5])
+
+
+# a="yogeshswami"
+# for i in range(2,len(a),4 ):
+#     print(a[i:i+4])
+
+# a="imx_yogesh"
+# for i in range(0,len(a),4):
+    # print(a[i:i+3])
+
+#for i in range (5):
+#    pass
+#    print("hello")
+
+
